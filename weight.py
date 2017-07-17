164ef9161ce0e2e1dd80910a58a8d6ed8b539666
F_Haga <s1240056@gmail.com>
2017-07-17 21:53:31 +0900

    update weight

--- weight.py
+++ weight.py
@@ -30,10 +30,12 @@ day_sum = len(df.index)
 minweight = min(df['体重'])
 sabun = round(minweight, 5) - weight
 sabun = round(sabun,5)
-print("体重の差は {} kg です.".format(sabun,5))
+BMI = round(weight/(1.735*1.735),5)
+print("体重の差は {0} kg です.BMIは{1}ですよ〜！".format(sabun,BMI))
+
 # print(hiduke, day_sum, weight, sabun)
 
-df2 = pd.Series([day_sum,hiduke,weight,sabun,fat],index=['日数','日付','体重','差分','体脂肪率'])
+df2 = pd.Series([day_sum,hiduke,weight,sabun,fat,BMI],index=['日数','日付','体重','差分','体脂肪率','BMI'])
 df = df.append(df2,ignore_index=True)
 
 df.to_csv('weightdata.csv')
